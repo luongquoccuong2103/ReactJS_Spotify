616fcd84eb6a6d22f75171ec6b73393c66c783f6
prothegod <quangduattran@gmail.com>
2022-09-15 16:13:10 +0700

    feat: add browse&toplists
    add browse demo and toplists demo(will work on API later)

---
+++ src/components/layouts/Pages/Browse/TopLists/index.tsx
@@ -0,0 +1,57 @@
+import "./index.css";
+
+const TopList = () => {
+  return (
+    <>
+      <div className="main-view">
+        <div>
+          <div className="content-spacing">
+            <h2 className="mb-4 text-heading">Top Lists</h2>
+            <div className="block">
+              <div className="common-grid">
+                {/* call api */}
+                <div className="contented">
+                  <a className="flex flex-col flex-1" href="#">
+                    <div className="media-cover">
+                      <div
+                        className="mb-4 media-cover-2"
+                        style={{
+                          borderRadius: "initial",
+                          backgroundImage: `url(https://i.scdn.co/image/ab67706f00000003e8641a419c367f9504f587aa)`,
+                        }}
+                      ></div>
+                      <div className="play-button-overlay">
+                        <div className="flex play-icon control-button large text-white bg-primary">
+                          <div className="play-icon svgicon text-[1.5rem]">
+                            <svg
+                              viewBox="0 0 16 16"
+                              xmlns="http://www.w3.org/2000/svg"
+                              height="100%"
+                              width="100%"
+                              preserveAspectRatio="xMidYMid meet"
+                              focusable="false"
+                            >
+                              <path d="M4.018 14L14.41 8 4.018 2z"></path>
+                            </svg>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    {/* description */}
+                    <div className='font-bold text-white ellipsis-one-line hover:text-white'>
+                    Today's Top Hits 
+                    </div>
+                    <div className="text-description"> Nicki Minaj is on top of the Hottest 50! </div>
+                  </a>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TopList;
---
+++ src/components/layouts/Pages/Browse/index.tsx
@@ -0,0 +1,98 @@
+import "./index.css";
+
+const BodyBrowse = () => {
+  return (
+    <>
+      <div className="main-view">
+        <div>
+          <div className="mb-6 px-[2rem] pt-[1rem]">
+            <div className="common-grid">
+              {/* noi dung sau de import API */}
+              <div>
+                <a className="flex relative ul" id='hv'>
+                  <div
+                    className="image"
+                    style={{
+                      backgroundImage: `url(https://t.scdn.co/media/derived/toplists_11160599e6a04ac5d6f2757f5511778f_0_0_275_275.jpg)`,
+                    }}
+                  ></div>
+                  <div className="category-name ellipsis-one-line txunderline">
+                    Top Lists
+                  </div>
+                </a>
+              </div>
+              <div>
+                <a className="flex relative ul" id='hv'>
+                  <div
+                    className="image"
+                    style={{
+                      backgroundImage: `url(https://t.scdn.co/media/derived/toplists_11160599e6a04ac5d6f2757f5511778f_0_0_275_275.jpg)`,
+                    }}
+                  ></div>
+                  <div className="category-name ellipsis-one-line txunderline">
+                    Top Lists
+                  </div>
+                </a>
+              </div>
+              <div>
+                <a className="flex relative ul" id='hv'>
+                  <div
+                    className="image"
+                    style={{
+                      backgroundImage: `url(https://t.scdn.co/media/derived/toplists_11160599e6a04ac5d6f2757f5511778f_0_0_275_275.jpg)`,
+                    }}
+                  ></div>
+                  <div className="category-name ellipsis-one-line txunderline">
+                    Top Lists
+                  </div>
+                </a>
+              </div>
+              <div>
+                <a className="flex relative ul" id='hv'>
+                  <div
+                    className="image"
+                    style={{
+                      backgroundImage: `url(https://t.scdn.co/media/derived/toplists_11160599e6a04ac5d6f2757f5511778f_0_0_275_275.jpg)`,
+                    }}
+                  ></div>
+                  <div className="category-name ellipsis-one-line txunderline">
+                    Top Lists
+                  </div>
+                </a>
+              </div>
+              <div>
+                <a className="flex relative ul" id='hv'>
+                  <div
+                    className="image"
+                    style={{
+                      backgroundImage: `url(https://t.scdn.co/media/derived/toplists_11160599e6a04ac5d6f2757f5511778f_0_0_275_275.jpg)`,
+                    }}
+                  ></div>
+                  <div className="category-name ellipsis-one-line txunderline">
+                    Top Lists
+                  </div>
+                </a>
+              </div>
+              <div>
+                <a className="flex relative ul" id='hv'>
+                  <div
+                    className="image"
+                    style={{
+                      backgroundImage: `url(https://t.scdn.co/media/derived/toplists_11160599e6a04ac5d6f2757f5511778f_0_0_275_275.jpg)`,
+                    }}
+                  ></div>
+                  <div className="category-name ellipsis-one-line txunderline">
+                    Top Lists
+                  </div>
+                </a>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BodyBrowse;
