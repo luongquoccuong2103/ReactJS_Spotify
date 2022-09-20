0788d44561f89cd3c303b9be5ae851509b500280
prothegod <quangduattran@gmail.com>
2022-09-20 10:09:10 +0700

    feat: update likedsong button
    fix button hover in likedsong

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        navBar: "232px",
+      },
+
+      height: {
+        topBar: "60px",
+        nowPlayingBar: "90px",
+      },
+
+      colors: {
+        primary: "#1db954",
+        sliderRail: "#535353",
+        sliderTrack: "#b3b3b3",
+      },
+    },
   },
   plugins: [
     function ({ addVariant }) {
-      addVariant('child', '& > *');
-      addVariant('child-hover', '& > *:hover');
-    }
+      addVariant("child", "& > *");
+      addVariant("child-hover", "& > *:hover");
+    },
   ],
-}
+};
