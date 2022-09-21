68a39a115d526d76973afe428508db530a53f0e5
luongquoccuong2103 <84918085+luongquoccuong2103@users.noreply.github.com>
2022-09-21 10:57:28 +0700

    commit

--- src/components/MusicCard/MusicCard.tsx
+++ src/components/MusicCard/MusicCard.tsx
@@ -2,11 +2,11 @@ import './MusicCard.css';
 
 const MusicCard = (props: any) => {
   return (
-    <div className="contented max-h-[240px]">
+    <div className="contented  max-h-[240px]">
       <a className="flex flex-col flex-1" href="#">
         <div className="media-cover">
           <div
-            className="mb-4 media-cover-2"
+            className="mb-4 media-cover-2 bg-white"
             style={{
               borderRadius: 'initial',
               // backgroundImage: `url(https://i.scdn.co/image/ab67616d0000b273d1241debb8543af8322a7d6a)`
--- src/views/Home/HomeBody.tsx
+++ src/views/Home/HomeBody.tsx
@@ -5,6 +5,8 @@ import './HomeBody.css';
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Album from '../Album/album';
+import MusicCard from '../../components/MusicCard/MusicCard';
+import Play from '../../components/assets/image/MyAlbum/play';
 const HomeBody = () => {
   return (
     <React.Fragment>
@@ -21,73 +23,24 @@ const HomeBody = () => {
         <div className="recently-played">
           <h2 className="mt-8 mb-4 text-heading">Recently Played</h2>
           <div className="content-grid">
-            <div className="media">
-              <div className="media-content">
-                <img
-                  src="https://i.scdn.co/image/ab67616d0000b2735888c34015bebbf123957f6d"
-                  className="media-image"
-                />
-                <div
-                  aria-roledescription="button"
-                  title="Play"
-                  className="flex play-icon control-button large text-white bg-primary rounded-full play-button-overlay"
-                >
-                  <svg
-                    viewBox="0 0 16 16"
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="60%"
-                    width="60%"
-                    preserveAspectRatio="xMidYMid meet"
-                    focusable="false"
-                  >
-                    <path d="M4.018 14L14.41 8 4.018 2z"></path>
-                  </svg>
-                </div>
-                <div className="font-bold text-white ellipsis-one-line hover:text-white">
-                  {' '}
-                  Chúng Ta Của Hiện Tại{' '}
-                </div>
-                <div className="text-description"> Sơn Tùng M-TP </div>
-              </div>
-            </div>
+            <MusicCard
+              url="https://i.scdn.co/image/ab67616d0000b2735888c34015bebbf123957f6d"
+              img={<Play />}
+              mediades={'Son Tung'}
+              description={'test'}
+            />  
           </div>
         </div>
       </Link>
       <div className="featured-playlists">
         <h2 className="mt-8 mb-4 text-heading">Ngủ ngon, mơ đẹp!.</h2>
         <div className="content-grid">
-          <div className="media">
-            <div className="media-content">
-              <img
-                src="https://i.scdn.co/image/ab67706f00000003cc785c1541d8b540da816688"
-                className="media-image"
-              />
-              <div
-                aria-roledescription="button"
-                title="Play"
-                className="flex play-icon control-button large text-white bg-primary rounded-full play-button-overlay"
-              >
-                <svg
-                  viewBox="0 0 16 16"
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="60%"
-                  width="60%"
-                  preserveAspectRatio="xMidYMid meet"
-                  focusable="false"
-                >
-                  <path d="M4.018 14L14.41 8 4.018 2z"></path>
-                </svg>
-              </div>
-              <div className="font-bold text-white ellipsis-one-line hover:text-white">
-                {' '}
-                Chúng Ta Của Hiện Tại{' '}
-              </div>
-              <div className="text-description">
-                {' '}
-                Những giai điệu êm dịu nhất cho giấc ngủ trọn vẹn.{' '}
-              </div>
-            </div>
-          </div>
+          <MusicCard
+            url="https://i.scdn.co/image/ab67706f00000003cc785c1541d8b540da816688"
+            img={<Play />}
+            mediades={'Son Tung'}
+            description={'test'}
+          />
         </div>
       </div>
       <Routes>
