ee785c208c1ffe05993199238b42e5309567900e
prothegod <quangduattran@gmail.com>
2022-09-23 11:17:44 +0700

    feat: add callAPI

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from 'react';
 
 const CLIENT_ID = 'fdb817872fb14108bb1802655f229199'; // insert your client id here from spotify
 const SPOTIFY_AUTHORIZE_ENDPOINT = 'https://accounts.spotify.com/authorize';
-const REDIRECT_URL_AFTER_LOGIN = 'http://localhost:3000';
+const REDIRECT_URL_AFTER_LOGIN = 'http://localhost:3000/ReactJS_Spotify';
 const SPACE_DELIMITER = '%20';
 const SCOPES = ['user-read-currently-playing', 'user-read-playback-state', 'playlist-read-private'];
 const SCOPES_URL_PARAM = SCOPES.join(SPACE_DELIMITER);
@@ -25,27 +25,24 @@ const getReturnedParamsFromSpotifyAuth = (hash: any) => {
 };
 
 function App() {
+  const token = localStorage.getItem('accessToken');
   useEffect(() => {
     if (window.location.hash) {
       const { access_token, expires_in, token_type } = getReturnedParamsFromSpotifyAuth(
         window.location.hash
       );
 
-      // localStorage.clear();
+      localStorage.clear();
 
       localStorage.setItem('accessToken', access_token);
       localStorage.setItem('tokenType', token_type);
       localStorage.setItem('expiresIn', expires_in);
     }
-  });
-
-  const token = localStorage.getItem('accessToken');
-  console.log(token);
+  }, []);
 
   if (!token) {
     window.location.href = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
   }
-
   const handleLogin = () => {
     window.location.href = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
   };
---
+++ src/components/callAPI/CallAPI.tsx
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import MusicCard from '../MusicCard/MusicCard';
+
+const CallAPI = (props: any) => {
+  const [token, setToken] = useState('');
+  const [data, setData]: any = useState();
+
+  useEffect(() => {
+    const call = async () => {
+      await axios
+        .get(props.endpoint, {
+          headers: {
+            Authorization: 'Bearer ' + token
+          }
+        })
+        .then((response) => {
+          setData(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+
+      if (localStorage.getItem('accessToken')) {
+        setToken(localStorage.getItem('accessToken')!);
+      }
+    };
+
+    call();
+  }, [token]);
+
+  return (
+    <>
+      {data?.items
+        ? data.items.map((item: any) => (
+            <>
+              <MusicCard
+                key={item.id}
+                mediades={item.name}
+                description={item.description}
+                url={item.images[0].url}
+              />
+            </>
+          ))
+        : null}
+    </>
+  );
+};
+
+export default CallAPI;
